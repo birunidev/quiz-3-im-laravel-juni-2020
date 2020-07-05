e607aea46ea5ba9871d60b4426b2cf7347ac06c3
birunidev <birunimamak@gmail.com>
2020-07-05 10:11:28 +0700

    add migration

---
+++ database/migrations/2020_07_05_030221_create_follower_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follower', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('profile_id')->references('id')->on('profile');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('follower');
+    }
+}
