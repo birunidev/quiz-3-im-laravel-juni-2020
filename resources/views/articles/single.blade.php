acc7581c0e9a5c2e6eaf912f188cbe5eaa8b6e76
birunidev <birunimamak@gmail.com>
2020-07-05 19:14:01 +0700

    add delete

--- resources/views/articles/single.blade.php
+++ resources/views/articles/single.blade.php
@@ -8,6 +8,8 @@
   <div class="col-md-12">
     <h1>{{$article->judul}}</h1>
     <a class="float-right" href="/articles/{{$article->id}}/edit">Edit Artikel</a>
+    <a class="float-right" href="/articles/{{$article->id}}">Hapus Artikel</a>
+
     <div class="d-flex">
       <small class="mr-4 d-block">Tanggal diubah: {{$article->updated_at}}</small>
       <small>Tanggal dibuat: {{$article->created_at}}</small>
